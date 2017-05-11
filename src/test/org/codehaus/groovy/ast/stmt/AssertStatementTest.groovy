f0850d33d9edf72b1b4f0764fe7805223624f8c9
dkhamitov <khamitov.dm@gmail.com>
2017-05-11 09:46:38 +0300

    Initial commit
    * test app

---
+++ src/test/org/codehaus/groovy/ast/stmt/AssertStatementTest.groovy
@@ -0,0 +1,41 @@
+package org.codehaus.groovy.ast.stmt
+
+import groovy.transform.CompileStatic
+import org.codehaus.groovy.control.SourceUnit
+import org.junit.Test
+
+@CompileStatic
+class AssertStatementTest {
+    @Test
+    void test() {
+//        def src = 'assert 1 == 2'
+//        def src = 'assert 1 = 2'
+        /*
+        def src = '''
+            assert a = 42
+            println('a = ' + a)
+
+            if (b = -42) {
+                println('b = ' + b)
+            }
+
+            if (c = 0) {
+                println('c = ' + c)
+            }
+        '''
+        */
+        def src = 'assert 1 } 2'
+        def srcUnit = SourceUnit.create('dummy', src)
+        parse(srcUnit)
+        def ast = srcUnit.getAST()
+
+        println(ast)
+    }
+
+    static void parse(SourceUnit src) {
+        src.parse()
+        src.completePhase()
+        src.nextPhase()
+        src.convert()
+    }
+}
